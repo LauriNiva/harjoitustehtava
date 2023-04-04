b1b33f1e142fa134292cc484fd3aa7c0190a7b5a
Lauri Niva <niva.lauri@gmail.com>
2023-04-04 16:23:33 +0300

    bugikorjaus

--- src/helpers/useForm.js
+++ src/helpers/useForm.js
@@ -23,14 +23,17 @@ export default function useForm(initial = {}) {
     });
   }
 
-  function resetForm() {
-    setInputs(initial);
-  }
+ function clearForm() {
+   const blankState = Object.fromEntries(
+     Object.entries(inputs).map(([key, value]) => [key, ''])
+   );
+   setInputs(blankState);
+ }
 
 
   return {
     inputs,
     handleChange,
-    resetForm,
+    clearForm,
   };
 }
